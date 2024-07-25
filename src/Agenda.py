cd91709da0961df73bc5939d8ada4970ce0663bb
Daniel Sousa <sousadanielsb@gmail.com>
2024-07-25 18:51:59 -0300

    Add: Ciclo 0 - Sair

---
+++ src/Agenda.py
@@ -0,0 +1,7 @@
+class Agenda:
+    def __init__(self):
+        pass
+
+    # Ciclo 0
+    def sair(self):
+        return 0
---
+++ tests/test_sair.py
@@ -0,0 +1,6 @@
+from src.Agenda import Agenda
+
+# Ciclo 0
+def test_sair():
+    agenda = Agenda()
+    assert agenda.sair() == 0
