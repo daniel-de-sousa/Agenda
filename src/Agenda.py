a5fca0c1e445a2a713d9cc214f6cd946a80c0a8a
Daniel Sousa <sousadanielsb@gmail.com>
2024-07-25 19:01:46 -0300

    Add: Ciclo 2 - Adicionar com Sucesso

--- src/Agenda.py
+++ src/Agenda.py
@@ -1,6 +1,21 @@
+import datetime
 class Agenda:
     def __init__(self):
+        self.eventos = []
         pass
+    
+    # Ciclo 2
+    def add_Evento(self, nome, inicio, fim):
+        inicio_dt = datetime.datetime.strptime(inicio, "%Y-%m-%d %H:%M")
+        fim_dt = datetime.datetime.strptime(fim, "%Y-%m-%d %H:%M")
+        
+        self.eventos.append({
+            'nome': nome,
+            'inicio': inicio_dt,
+            'fim': fim_dt
+        })
+
+        print("Evento adicionado com sucesso.")
 
     # Ciclo 1
     def sair(self):
---
+++ tests/test_adicionar.py
@@ -0,0 +1,17 @@
+import pytest
+from datetime import datetime
+from src.Agenda import Agenda
+
+@pytest.fixture
+def agenda():
+    return Agenda()
+
+# Ciclo 2
+def test_adicionar_Evento_Sucesso(agenda, capsys):
+    agenda.add_Evento("Final dos 100m", "2024-07-24 10:00", "2024-07-24 11:00")
+    out, _ = capsys.readouterr()
+    assert out.strip() == "Evento adicionado com sucesso."
+    assert len(agenda.eventos) == 1
+    assert agenda.eventos[0]['nome'] == "Final dos 100m"
+    assert agenda.eventos[0]['inicio'] == datetime(2024, 7, 24, 10, 0)
+    assert agenda.eventos[0]['fim'] == datetime(2024, 7, 24, 11, 0)
